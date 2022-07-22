28ae3736a355e8f81de71455388bfe41e357c207
Aymane Aggoujjil <aaggoujj@student.1337.ma>
2022-07-22 21:29:02 +0100

    chore: add prototype

--- include/minishell.h
+++ include/minishell.h
@@ -6,14 +6,26 @@
 /*   By: aaggoujj <aaggoujj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 08:12:45 by aaggoujj          #+#    #+#             */
-/*   Updated: 2022/07/22 08:13:15 by aaggoujj         ###   ########.fr       */
+/*   Updated: 2022/07/22 20:47:36 by aaggoujj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <unistd.h>
-#include <stdio.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <dirent.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "../lib/libft/include/libft.h"
+
+void	sigint_handler(int sig);
+void	_ctrl_handler(void);
+void	ctrl_d_handler(void);
 
 #endif /* MINISHELL_H */
