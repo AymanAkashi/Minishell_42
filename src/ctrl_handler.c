45d67cf52171b245655344ce488261e6b48db215
Aymane Aggoujjil <aaggoujj@student.1337.ma>
2022-07-22 21:28:04 +0100

    feat: signal handling

---
+++ src/ctrl_handler.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ctrl_handler.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaggoujj <aaggoujj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/22 20:12:24 by aaggoujj          #+#    #+#             */
+/*   Updated: 2022/07/22 20:48:02 by aaggoujj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	sigint_handler(int sig)
+{
+	(void)sig;
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+void	_ctrl_handler(void)
+{
+	rl_catch_signals = 0;
+	signal(SIGINT, sigint_handler);
+	signal(SIGQUIT, SIG_IGN);
+}
+
+void	ctrl_d_handler(void)
+{
+	printf("\x1B[1;A\x1B[13;Cexit\n");
+	rl_clear_history();
+	exit(EXIT_SUCCESS);
+}
