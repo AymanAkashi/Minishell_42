bb575820623b0b281a53d9a74ce6e3322e1ffcf7
Aymane Aggoujjil <aaggoujj@student.1337.ma>
2022-07-22 21:27:00 +0100

    feat: create prompt loop

--- src/minishell.c
+++ src/minishell.c
@@ -6,19 +6,31 @@
 /*   By: aaggoujj <aaggoujj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 08:07:03 by aaggoujj          #+#    #+#             */
-/*   Updated: 2022/07/22 08:14:05 by aaggoujj         ###   ########.fr       */
+/*   Updated: 2022/07/22 20:48:34 by aaggoujj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main() {
-  char cwd[256];
+int	main(void)
+{
+	char	*line;
 
-
-    if (getcwd(cwd, sizeof(cwd)) == NULL)
-      perror("getcwd() error");
-    else
-      printf("current working directory is: %s\n", cwd);
+	while (1)
+	{
+		_ctrl_handler();
+		line = readline("Minishell $> ");
+		if (line != NULL)
+		{
+			add_history(line);
+			if (ft_strncmp(line, "exit", 5) == 0)
+				break ;
+			free(line);
+		}
+		else
+		{
+			ctrl_d_handler();
+		}
+	}
 	return (0);
 }
