2c9320ece9c93f4815a08ad7a699dd44b2ec724c
Aymane Aggoujjil <aaggoujj@student.1337.ma>
2022-07-23 23:17:39 +0100

    chore:add prototype

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: aaggoujj <aaggoujj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 08:12:45 by aaggoujj          #+#    #+#             */
-/*   Updated: 2022/07/22 20:47:36 by aaggoujj         ###   ########.fr       */
+/*   Updated: 2022/07/23 23:11:18 by aaggoujj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,39 @@
 # include <readline/history.h>
 # include "../lib/libft/include/libft.h"
 
+
+typedef enum e_type_token{
+	TOKEN_WORD,
+	TOKEN_SEPARATOR,
+	TOKEN_RED2_OUT,
+	TOKEN_PIPE,
+	TOKEN_RED_IN,
+	TOKEN_RED_OUT,
+	TOKEN_HEREDOC,
+	TOKEN_AND,
+	TOKEN_OR,
+} t_type_token;
+typedef struct s_token
+{
+	t_type_token type_token;
+	char			*cmd;
+	int				type;
+	int				index;
+	int				len;
+	int				start;
+	struct s_token	*next;
+}		t_token;
+//****************************************************************
+
+int		token_and(t_token *token, char *line, int i);
+int		token_paren(t_token *token, char *line, int i);
+int		token_red_in(t_token *token, char *line, int i);
+int		token_red_out(t_token *token, char *line, int i);
+
+//****************************************************************
 void	sigint_handler(int sig);
 void	_ctrl_handler(void);
 void	ctrl_d_handler(void);
+void	tokenizetion(t_token **token, char *line);
 
 #endif /* MINISHELL_H */
