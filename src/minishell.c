6f2080c8a753cc53b9953ff7953245ec5c13fd2c
Aymane Aggoujjil <aaggoujj@student.1337.ma>
2022-07-23 23:15:19 +0100

    feat: tokenization part start

--- src/minishell.c
+++ src/minishell.c
@@ -6,15 +6,30 @@
 /*   By: aaggoujj <aaggoujj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 08:07:03 by aaggoujj          #+#    #+#             */
-/*   Updated: 2022/07/22 20:48:34 by aaggoujj         ###   ########.fr       */
+/*   Updated: 2022/07/23 23:13:09 by aaggoujj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	free_token(t_token **token)
+{
+	t_token	*tmp;
+
+	while (*token != NULL)
+	{
+		tmp = *token;
+		*token = (*token)->next;
+		free(tmp);
+	}
+	*token = NULL;
+}
+
 int	main(void)
 {
 	char	*line;
+	t_token	*token;
+	t_token	*tmp;
 
 	while (1)
 	{
@@ -22,15 +37,19 @@ int	main(void)
 		line = readline("Minishell $> ");
 		if (line != NULL)
 		{
+			token = (t_token *)malloc(sizeof(t_token));
+			token->cmd = NULL;
+			token->next = NULL;
+			tokenizetion(&token, line);
+			tmp = token;
 			add_history(line);
 			if (ft_strncmp(line, "exit", 5) == 0)
 				break ;
 			free(line);
+			free_token(&token);
 		}
 		else
-		{
 			ctrl_d_handler();
-		}
 	}
 	return (0);
 }
