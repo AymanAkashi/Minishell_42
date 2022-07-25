c38cf684b8412d18a3acb5a740c92b968c8b9a72
AymanAkashi <aaggoujj@student.1337.ma>
2022-07-25 21:28:47 +0100

    feat: fonction part scanner

--- src/scanner.c
+++ src/scanner.c
@@ -6,7 +6,7 @@
 /*   By: aaggoujj <aaggoujj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 20:26:53 by aaggoujj          #+#    #+#             */
-/*   Updated: 2022/07/24 23:54:21 by aaggoujj         ###   ########.fr       */
+/*   Updated: 2022/07/25 08:28:59 by aaggoujj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,22 @@ t_token	*get_next_token(t_token *token)
 	return (NULL);
 }
 
-t_scanner	*scanner_token(t_token *token, t_scanner *curr_scan)
+void	scanner_token(t_token *token, t_scanner **curr_scan)
 {
-	t_scanner	*scanner;
-
-	scanner = (t_scanner *)malloc(sizeof(t_scanner));
-	scanner->curr_token = NULL;
-	scanner->next_token = NULL;
-	if (curr_scan == NULL)
+	if ((*curr_scan) == NULL)
 	{
-		scanner->curr_token = token;
-		scanner->next_token = get_next_token(token);
-		return (scanner);
+		*curr_scan = (t_scanner *)malloc(sizeof(t_scanner));
+		(*curr_scan)->curr_token = token;
+		(*curr_scan)->next_token = get_next_token(token);
 	}
-	while (token && token != curr_scan->curr_token)
-		token = token->next;
-	if (token)
+	else
 	{
-		scanner->curr_token = token;
-		scanner->next_token = get_next_token(token);
+		while (token && token != (*curr_scan)->next_token)
+			token = token->next;
+		if (token)
+		{
+			(*curr_scan)->curr_token = token;
+			(*curr_scan)->next_token = get_next_token(token);
+		}
 	}
-	return (scanner);
 }
