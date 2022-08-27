f4a11eddb76c8cdb33539965487e81f739f92498
AymanAkashi <aaggoujj@student.1337.ma>
2022-08-27 19:19:27 +0100

    feat: add export

--- src/env_fonction.c
+++ src/env_fonction.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env_fonction.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaggoujj <aaggoujj@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: aaggoujj <aaggoujj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 18:07:51 by aaggoujj          #+#    #+#             */
-/*   Updated: 2022/08/22 19:01:04 by aaggoujj         ###   ########.fr       */
+/*   Updated: 2022/08/27 19:18:01 by aaggoujj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void	alloc_empty_envp(t_data *data)
 	data->envp = ft_lstnew(ft_env_new("PWD",pwd));
 	ft_lstadd_back(&data->envp, ft_lstnew(ft_env_new("OLDPWD",NULL)));
 	ft_lstadd_back(&data->envp, ft_lstnew(ft_env_new("SHLVL","1")));
-	ft_lstadd_back(&data->envp, ft_lstnew(ft_env_new("_",ft_strjoin(pwd, "/minishell"))));
+	ft_lstadd_back(&data->envp, ft_lstnew(ft_env_new("_","/usr/bin/env")));
 }
 
 void	alloc_envp(t_data *data, char *envp[])
@@ -50,8 +50,10 @@ void	alloc_envp(t_data *data, char *envp[])
 	t_list *head;
 	char *tmp;
 	t_env *e;
+	int		o;
 
 	i = 1;
+	o = 0;
 	if (!*envp)
 		alloc_empty_envp(data);
 	else
@@ -65,38 +67,74 @@ void	alloc_envp(t_data *data, char *envp[])
 		{
 			tmp = ft_strchr(envp[i], '=') + 1;
 			*ft_strchr(envp[i], '=') = '\0';
-			e = ft_env_new(envp[i], tmp);
+			if (!ft_strcmp(envp[i], "OLDPWD"))
+			{
+				e = ft_env_new(envp[i], "");
+				o = 1;
+			}
+			else
+				e = ft_env_new(envp[i], tmp);
 			ft_lstadd_back(&head, ft_lstnew(e));
 			i++;
 		}
-			// free(tmp);
+		if (!o)
+		{
+			ft_env_new("OLDPWD", "");
+			ft_lstadd_back(&head, ft_lstnew(e));
+		}
 		ft_lstadd_back(&head, NULL);
 		data->envp = head;
 	}
 }
 
-void	sort_list(t_list *lst, t_list **lst2)
+void	print_export(char *name, char *value, t_env **e)
 {
-	t_list *tmp;
-	t_env	*name;
-	t_env	*name2;
+	(*e)->print = 1;
+	printf("declare -x %s", name);
+	if (value && value[0] != '\0')
+		printf("=\"%s\"\n", value);
+	else
+		printf("\n");
+}
 
-	(*lst2) = lst;
-	tmp = lst->next;
+char *no_print(t_list *lst)
+{
+	t_env *e;
 	while(lst)
 	{
-		tmp = lst->next;
+		e = lst->content;
+		if(e->print == 0)
+			return (e->name);
+		lst = lst->next;
+	}
+	return (NULL);
+}
+
+void	sort_list(t_list *lst, t_list *head)
+{
+	char	*key;
+	char	*value;
+	t_list *tmp;
+	t_env	*name;
+	t_env   *min;
+
+	while(lst)
+	{
+		tmp = head;
+		name = tmp->content;
+		key = no_print(tmp);
 		while (tmp)
 		{
-			name = lst->content;
-			name2 = tmp->content;
-			if (ft_strcmp(name->name, name2->name) > 0)
+			name = tmp->content;
+			if (ft_strcmp(key, name->name) >= 0 && name->print == 0)
 			{
-				tmp->content = name;
-				lst->content = name2;
+				min = name;
+				key = name->name;
+				value = name->value;
 			}
 			tmp = tmp->next;
 		}
+		print_export(key, value, &min);
 		lst = lst->next;
 	}
 }
@@ -124,20 +162,7 @@ t_list	*ft_lstcopy(t_list *lst)
 
 void	ft_export_new(t_data *data)
 {
-	data->export = ft_any_alloc(sizeof(t_list), 1);
-	sort_list(data->envp, &data->export);
-	//***********************export**************************//
-	// t_env *e;
-	// t_list *lst;
-	// lst = data->export;
-	// for (int i = 0;data->export; i++)
-	// {
-	// 	e = data->export->content;
-	// 	printf("[name]%s === [value]%s\n", e->name, e->value);
-	// 	data->export = data->export->next;
-	// }
-	// data->export = lst  ;
-	//************************ *****************************//
+	sort_list(data->envp, data->envp);
 }
 
 t_env	*ft_env_new(char *name, char *value)
