45d506fcafe6b4d21ebf8a19e00621c3e61fb91d
Aymane Aggoujjil <aaggoujj@student.1337.ma>
2022-09-21 00:16:42 +0100

    feat: exit still have case

---
+++ src/ft_exit.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_exit.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaggoujj <aaggoujj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 22:17:28 by aaggoujj          #+#    #+#             */
+/*   Updated: 2022/09/21 00:13:45 by aaggoujj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	check_digit(char *str)
+{
+	int i;
+
+	i = -1;
+	while (str[++i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+	}
+	return (1);
+}
+
+void	ft_exit(char **args)
+{
+	if(args[1] == NULL)
+		exit(0);
+	if (check_digit(args[1]) && args[2] != NULL)
+	{
+		ft_putstr_fd("exit\n", 2);
+		ft_putstr_fd("minishell: exit: too many arguments\n", 2);
+		exit(ft_atoi(args[1]));
+	}
+	else if (!check_digit(args[1]))
+	{
+		ft_putstr_fd("exit\n", 2);
+		ft_putstr_fd("minishell: exit: asadf: numeric argument required\n", 2);
+		exit(2);
+	}
+	else
+	{
+		ft_putstr_fd("exit\n", 2);
+		exit(ft_atoi(args[1]));
+	}
+}
